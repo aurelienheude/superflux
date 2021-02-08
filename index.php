e035ec1de5ad52c4e28b981ff510aaee2411cc47
aurelienheude <aurelienheudepro@gmail.com>
2021-02-08 15:31:38 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include "controller_index.php"; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,9 +8,9 @@
     <title>SUPER FLUX</title>
     
     <!-- PAGE CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.css">
     <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
