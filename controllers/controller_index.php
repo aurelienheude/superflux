2108366b1c625ab10b9a1980ade0aa10bfcd60ed
William Touboul <will.touboul@gmail.com>
2021-02-09 09:44:27 +0100

    AJout fonction card

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -1,6 +1,12 @@
 <?php 
 
-
+$feedArray = [
+    'https://www.01net.com/rss/pc-portables/' => "Flux PC Portable",
+    'https://www.01net.com/rss/tests/les-derniers-tests/rss-derniers-tests/' => 'Flux Tests',
+    'https://www.01net.com/rss/actualites/applis-logiciels/' => 'Flux Applis & Logiciels',
+    'https://www.01net.com/rss/actualites/jeux/' => 'Flux Jeux',
+    'https://www.01net.com/rss/actualites/securite/' => 'Flux Securité'
+]; // Tableau avec les feeds pour le select, les $key seront les xml
 
 
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<?php include "controller_index.php"; ?>
+<?php include "controllers/controller_index.php"; ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SUPER FLUX</title>
-    
+
     <!-- PAGE CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
     <!-- FONT AWESOME -->
@@ -14,49 +15,64 @@
     <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <!-- J'ai mit un form pour tester mais je pense pas qu'on aura la meme chose -->
+    <form action="index.php" method="post">
+        <p> Nombre de posts </p>
+        <div>
+            <input type="radio" id="5" name="number" value="4" <?= isset($_POST['number']) && $_POST['number'] == '5' ? 'checked' : '' ?> ?>
+            <label for="5">5</label>
+        </div>
+
+        <div>
+            <input type="radio" id="10" name="number" value="9" <?= isset($_POST['number']) && $_POST['number'] == '10' ? 'checked' : '' ?> ?>
+            <label for="10">10</label>
+        </div>
+
+        <p>Feed</p>
+        <?php
+        foreach ($feedArray as $key => $value) { ?>
+            <div>
+                <input type="radio" id="<?= $key ?>" name="feed" value="<?= $key ?>" <?= isset($_POST['feed']) && $_POST['feed'] == $key ? 'selected' : '' ?>>
+                <label for="<?= $value ?>"><?= $value ?></label>
+            </div>
+        <?php
+        }
+        ?>
+        </select>
+        <input type="submit" id="submit" value="submit" name="submit">
+    </form>
 
 
+    <?php
+    if (isset($_POST['submit'])) {
 
+        $number = $_POST['number'];
+        $feed = $_POST['feed'];
+        $xml = simplexml_load_file($feed); // On prend le bon feed.
+        if ($number > 0) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            for ($i = 0; $i <= $number; $i++) { // On prend le nombre de post choisi faut encore voir pour l'option "tout".
+    ?>
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= $xml->channel->item[$i]->enclosure['url'] ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $xml->channel->item[$i]->title ?></h5>
+                        <?php
+                        $toExplode = $xml->channel->item[$i]->description;
+                        $pieces = explode("<br/><br/>", $toExplode);
+                        ?>
+                        <p class="card-text"><?= $pieces[0] ?> <br>
+                            <?= $xml->channel->item[$i]->link->pubDate ?> </p>
+                        <a href="<?= $xml->channel->item[$i]->link ?>" target="_blank" class="btn btn-primary">Article</a>
+                    </div>
+                </div>
+    <?php
+            }
+        }
+    }
+    ?>
 
 
     <!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
@@ -66,4 +82,5 @@
     <script type="text/javascript" src="assets/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
+
 </html>
