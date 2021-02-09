f7e7f20147bb0faf7ded041bdef0600ca505ae18
aurelienheude <aurelienheudepro@gmail.com>
2021-02-09 11:07:23 +0100

    modal

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -76,55 +76,39 @@ class rss
                         </button>
                     </div>
 
-                    <div class="modal" id="<?= "modal[$i]" ?>" tabindex="-1" role="dialog">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title"><?= $xml->channel->item[$i]->title ?></h5>
-                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <img src="<?= $xml->channel->item[$i]->enclosure['url'] ?>" class="card-img-top" alt="...">
-
-                                    <?php
-                                    $toExplode = $xml->channel->item[$i]->description;
-                                    $pieces = explode("<br/><br/>", $toExplode);
-                                    ?>
-                                    <p class="card-text"><?= $pieces[0] ?> <br>
-                                        <?= $xml->channel->item[$i]->link->pubDate ?> </p>
-                                </div>
-                                <div class="modal-footer">
-                                    <a href="<?= $xml->channel->item[$i]->link ?>" target="_blank" class="btn btn-primary">Article</a>
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
 
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            Launch demo modal
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#<?= "modal-$i" ?>">
+                            <?= $xml->channel->item[$i]->title ?>
                         </button>
 
                         <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="<?= "modal-$i" ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <h5 class="modal-title"><?= $xml->channel->item[$i]->title ?></h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        ...
+                                        <img src="<?= $xml->channel->item[$i]->enclosure['url'] ?>" class="card-img-top" alt="...">
+
+                                        <?php
+                                        $toExplode = $xml->channel->item[$i]->description;
+                                        $pieces = explode("<br/><br/>", $toExplode);
+                                        ?>
+                                        <p class="card-text"><?= $pieces[0] ?> <br>
+                                            <?= $xml->channel->item[$i]->link->pubDate ?> </p>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <a href="<?= $xml->channel->item[$i]->link ?>" target="_blank" class="btn btn-primary">Article</a>
                                         <button type="button" class="btn btn-primary">Save changes</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
         <?php
                 }
