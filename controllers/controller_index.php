3a723345ac6f7547d926e3c261aac63577fa4395
WilliamTouboul <will.touboul@gmail.com>
2021-02-10 11:51:45 +0100

    theme fonctionnelle

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8,6 +8,19 @@ $feedArray = [
     'https://www.01net.com/rss/actualites/securite/' => 'Flux Securité'
 ]; // Tableau avec les feeds pour le select, les $key seront les xml
 
+
+$style = 'assets/css/style.css?rnd=132';
+if (isset($_POST['style'])) {
+    if ($_POST['style'] == 'style1') {
+        $style = 'assets/css/style.css?rnd=132';
+    } else if ($_POST['style'] == 'style2') {
+        $style = 'assets/css/style2.css?rnd=132';
+    } else if ($_POST['style'] == 'style3') {
+        $style = 'assets/css/style3.css?rnd=132';
+    } else {
+        $style = 'assets/css/style.css?rnd=132';
+    }
+}
 class rss
 {
     function rss_tools()
@@ -49,29 +62,32 @@ class rss
                                 <div class="modal_header modal-content">
                                     <div class="modal-header">
                                         <h5 class="text-light modal-title"><?= $xml->channel->item[$i]->title ?></h5>
-                                        <button type="button" class="btn_style close"  data-bs-dismiss="modal" aria-label="Close">
+                                        <button type="button" class="btn_style close" data-bs-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                     <div class="modal_body modal-body">
                                         <img src="<?= $xml->channel->item[$i]->enclosure['url'] ?>" class="card-img-top" alt="...">
                                         <br /><br />
-                                        
+
                                         <?php
                                         $toExplode = $xml->channel->item[$i]->description;
                                         $pieces = explode("<br/><br/>", $toExplode);
                                         $date = strtotime($xml->channel->item[$i]->pubDate);
                                         $date2 = strftime("%A %d %B", $date);
                                         ?>
-                                        <b><p class="text-center"><?= $date2  ?></p></b><br />
+                                        <b>
+                                            <p class="text-center"><?= $date2  ?></p>
+                                        </b><br />
 
-                                        <b><p class="card-text"> <?= $pieces[0] ?> </p></b><br />
-                                        
+                                        <b>
+                                            <p class="card-text"> <?= $pieces[0] ?> </p>
+                                        </b><br />
+
 
                                     </div>
                                     <div class="modal_footer modal-footer">
                                         <a href="<?= $xml->channel->item[$i]->link ?>" target="_blank" class="btn btn_style">Article</a>
-                                        <button type="button" class="btn btn_style" data-bs-dismiss="modal">Close</button>
                                     </div>
                                 </div>
                             </div>
@@ -87,5 +103,4 @@ class rss
 <?php
     }
 }
-
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <title>SUPER FLUX</title>
 
     <!-- PAGE CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="<?= $style ?>">
+
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="assets/fontawesome/css/all.css">
 
@@ -20,81 +21,98 @@
     <!------------------------------------------------------------------------------------------------------------------------->
     <!------------------------------------------------------------------------------------------------------------------------->
     <!------------------------------------------------------------------------------------------------------------------------->
-        <nav class="navbar_style navbar" id="navbar" aria-label="First navbar example">
-            <div class="container-fluid">
-                <a id="branding" class="navbar_style_a navbar-brand" href="https://youtu.be/dQw4w9WgXcQ">SUPER FLUX</a>
-                <button class="navbar_style_button hover-no-border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="config_button fas fa-cog fa-2x"></i>
-                </button>
-            </div>
-        </nav>
-
-        <!------------------------------------------------------------------------------------------------------------------------->
-        <!------------------------------------------------------------------------------------------------------------------------->
-        <!------------------------------------------------------------------------------------------------------------------------->
-
-        <div class="collapse" id="navbarToggleExternalContent">
-            <form id="container_parent" class="form_style" action="index.php" method="post">
-
-                <div id="container_parent" class="container_parent container-fluid">
-                    <div class="row">
-                        <!---------------------------------------------------------------------------->
-                        <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
-                            <div class="inner text-center">
-                                <h2>Choisir un thème</h2>
-                                <div class="button_group">
-                                    <input id="white_mods_button" type="button" class="btn btn_style" onclick="white_mods();" value="LIGHT">
-                                    <input id="dark_mods_button" type="button" class="btn btn_style" onclick="dark_mods();" value="DARK">
-                                    <input id="default_button" type="button" class="btn btn_style" onclick="default_mods();" value="NORMAL">
-                                </div>
-
-                            </div>
-                        </div>
-                        <!---------------------------------------------------------------------------->
-                        <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
-                            <div class="inner text-center">
-                                <h2>Choisir un Flux</h2>
-                                <?php
-                                foreach ($feedArray as $key => $value) { ?>
-                                    <input type="radio" id="<?= $key ?>" name="feed" value="<?= $key ?>" <?= isset($_POST['feed']) && $_POST['feed'] == $key ? 'checked' : '' ?>>
-                                    <label for="<?= $value ?>"><?= $value ?></label>
-                                <?php } ?>
-
-                            </div>
-                        </div>
-                        <!---------------------------------------------------------------------------->
-                        <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
-                            <div class="inner text-center">
-                                <h2>Nombre de flux</h2>
-                                <div>
-                                    <input type="radio" id="5" name="number" value="4" <?= isset($_POST['number']) && $_POST['number'] == '5' ? 'checked' : '' ?> ?>
-                                    <label for="5">5</label>
-                                </div>
-
-                                <div>
-                                    <input type="radio" id="10" name="number" value="9" <?= isset($_POST['number']) && $_POST['number'] == '10' ? 'checked' : '' ?> ?>
-                                    <label for="10">10</label>
-                                </div>
-
-                                <div>
-                                    <input type="radio" id="tout" name="number" value="18" <?= isset($_POST['number']) && $_POST['number'] == '18' ? 'checked' : '' ?> ?>
-                                    <label for="tout">tout</label>
-                                </div>
-                                <input type="submit" class="btn btn_style" id="submit" value="SUBMIT" name="submit">
-                            </div>
-                        </div>
-                        <!--------------------------------------------------------------------------->
-                    </div>
-                </div>
-            </form>
+    <nav class="navbar_style navbar" id="navbar" aria-label="First navbar example">
+        <div class="container-fluid">
+            <a id="branding" class="navbar_style_a navbar-brand" href="https://youtu.be/dQw4w9WgXcQ">SUPER FLUX <br>
+                <marquee>Moins ca sert, mieux ca marche.</marquee>
+            </a>
+            <button class="navbar_style_button hover-no-border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="config_button fas fa-cog fa-2x"></i>
+            </button>
         </div>
+    </nav>
 
-        <div id="fake_body" class="fake_body container">
-            <?php
+    <!------------------------------------------------------------------------------------------------------------------------->
+    <!------------------------------------------------------------------------------------------------------------------------->
+    <!------------------------------------------------------------------------------------------------------------------------->
+
+    <div class="collapse" id="navbarToggleExternalContent">
+        <form id="container_parent" class="form_style" action="index.php" method="post">
+
+            <div id="container_parent" class="container_parent container-fluid">
+                <div class="row">
+                    <!---------------------------------------------------------------------------->
+                    <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
+                        <div class="inner text-center">
+
+                            <h2>theme</h2>
+                            <div>
+                                <input type="radio" id="style1" name="style" value="style1" <?= isset($_POST['style']) && $_POST['style'] == 'style1' ? 'checked' : '' ?> ?>
+                                <label for="5">Dark theme</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="style2" name="style" value="style2" <?= isset($_POST['style']) && $_POST['style'] == 'style2' ? 'checked' : '' ?> ?>
+                                <label for="5">Light theme</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="style3" name="style" value="style3" <?= isset($_POST['style']) && $_POST['style'] == 'style3' ? 'checked' : '' ?> ?>
+                                <label for="5">Moche theme</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!---------------------------------------------------------------------------->
+                    <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
+                        <div class="inner text-center">
+                            <h2>Choisir un Flux</h2>
+                            <?php
+                            foreach ($feedArray as $key => $value) { ?>
+                                <input type="radio" id="<?= $key ?>" name="feed" value="<?= $key ?>" <?= isset($_POST['feed']) && $_POST['feed'] == $key ? 'checked' : '' ?>>
+                                <label for="<?= $value ?>"><?= $value ?></label>
+                            <?php } ?>
+
+                        </div>
+                    </div>
+                    <!---------------------------------------------------------------------------->
+                    <div class="col-xl-4 col_child p-5 d-flex justify-content-center">
+                        <div class="inner text-center">
+                            <h2>Nombre de flux</h2>
+                            <div>
+                                <input type="radio" id="5" name="number" value="4" <?= isset($_POST['number']) && $_POST['number'] == '5' ? 'checked' : '' ?> ?>
+                                <label for="5">5</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="10" name="number" value="9" <?= isset($_POST['number']) && $_POST['number'] == '10' ? 'checked' : '' ?> ?>
+                                <label for="10">10</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="tout" name="number" value="18" <?= isset($_POST['number']) && $_POST['number'] == '18' ? 'checked' : '' ?> ?>
+                                <label for="tout">tout</label>
+                            </div>
+                            <input type="submit" class="btn btn_style" id="submit" value="SUBMIT" name="submit">
+                        </div>
+                    </div>
+                    <!--------------------------------------------------------------------------->
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div id="fake_body" class="fake_body container">
+        <div id="starshine">
+            <div class="template shine">
+                <?php
                 $obj_rss = new rss;
                 $obj_rss->rss_tools();
-            ?>
+                ?>
+            </div>
         </div>
+
+    </div>
 
     <!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
     <!----------------------------------------------------------------------------------------   FICHIER JS   ------------------------------------------------------------------------------->
